76e11ff6f8378212dd020cd11b64bba7b49c8444
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-12-17 22:52:15 +0100

    Update

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -11,7 +11,7 @@ class HomeScreen extends StatefulWidget {
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
-// Form
+// For
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
